392bbe4468dbfdd6c50511cb5394084caddaca35
David Fernandes da Costa <david.dacosta@gmail.com>
2024-07-25 19:38:17 -0300

    Aula statusBar customizada com menu.

--- app/src/main/java/com/example/instagram/login/view/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instagram/login/view/main/view/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.instagram.login.view.main.view
 
 import android.os.Bundle
 import android.view.WindowInsetsController
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.instagram.R
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
             WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
         window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
 
     }
 }
