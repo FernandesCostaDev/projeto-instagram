0670850fac5cf06b383d733ebb3153f42c65ae1b
David Fernandes da Costa <david.dacosta@gmail.com>
2024-07-25 19:10:51 -0300

    Aula statusBar programaticamente.

--- app/src/main/java/com/example/instagram/login/view/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instagram/login/view/main/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.instagram.login.view.main.view
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instagram.R
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //mudando a statusbar - só vai alterar a statusbar na tela principal, as outras telas continua normal
+        window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
 
     }
 }
