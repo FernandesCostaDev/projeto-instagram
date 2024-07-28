1ce101be0e828de0c4885f8eadd3b355894d2f52
David Fernandes da Costa <david.dacosta@gmail.com>
2024-07-28 17:19:20 -0300

    Finalizando primeiro modulo Instagram.

---
+++ app/src/main/java/com/example/instagram/login/view/camera/view/FragmentCamera.kt
@@ -0,0 +1,25 @@
+package com.example.instagram.login.view.camera.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagram.R
+
+class FragmentCamera: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+}
